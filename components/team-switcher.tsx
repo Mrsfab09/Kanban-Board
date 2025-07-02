5aafe074d1b28e3332c9ca301d9287eb50f88cf8
MrsFab09 <ytfabiskora07@gmail.com>
2025-07-02 18:35:52 +0200

    add components sidebar

---
+++ components/team-switcher.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Command } from "lucide-react";
+
+export function TeamSwitcher({}: {
+  teams: {
+    name: string;
+    logo: React.ElementType;
+    plan: string;
+  }[];
+}) {
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <div className="bg-muted text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <Command size={18} color="red" />
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">Kanban Board</span>
+              </div>
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
