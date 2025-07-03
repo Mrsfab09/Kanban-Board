6bdfb8570c8e812ebb2bc5b5235b97ec63fc4ab8
Fabian <office@email.com>
2025-07-03 18:40:48 +0200

    changed main page app

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,73 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { CardsDemo } from "@/components/custom/CardsDemo"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ChartNoAxesGantt, Presentation, ListTodo } from 'lucide-react';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <Tabs defaultValue="board" className="w-full">
+            <TabsList>
+              <TabsTrigger value="board">
+                <Presentation />
+                Board
+              </TabsTrigger>
+              <TabsTrigger value="timeline">
+                <ChartNoAxesGantt />
+                Timeline
+              </TabsTrigger>
+              <TabsTrigger value="list">
+                <ListTodo />
+                List
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="board"> 
+              <CardsDemo content="board"/>
+            </TabsContent>
+            <TabsContent value="timeline">Change your timeline here.</TabsContent>
+            <TabsContent value="list">Change your password here.</TabsContent>
+          </Tabs>
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
