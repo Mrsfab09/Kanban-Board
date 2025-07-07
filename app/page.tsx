4962aca959a9bced33a2364852e8d0752915d9a9
Fabian <office@email.com>
2025-07-07 20:05:15 +0200

    remove props

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
               </TabsTrigger>
             </TabsList>
             <TabsContent value="board">
-              <CardsDemo content="board" />
+              <CardsDemo />
             </TabsContent>
             <TabsContent value="timeline">
               Change your timeline here.
