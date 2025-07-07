f7fc8e989816e21da0ce49d39f603cb38bcf24d8
Fabian <office@email.com>
2025-07-07 20:15:47 +0200

    create new component ( TextareaDemo )

---
+++ components/custom/TextareaDemo.tsx
@@ -0,0 +1,18 @@
+import { Textarea } from "@/components/ui/textarea";
+
+export function TextareaDemo({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (val: string) => void;
+}) {
+  return (
+    <Textarea
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder="Type your message here."
+      style={{ height: "100px" }}
+    />
+  );
+}
