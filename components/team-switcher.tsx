bf625effe300ab3c3c66d02e3d530d1d54891ae8
MrsFab09 <ytfabiskora07@gmail.com>
2025-07-16 12:54:36 +0200

    change color icon

--- components/team-switcher.tsx
+++ components/team-switcher.tsx
@@ -28,7 +28,7 @@ export function TeamSwitcher({}: {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="bg-muted text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                <Command size={18} color="red" />
+                <Command size={18} className="text-orange-500" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">Kanban Board</span>
